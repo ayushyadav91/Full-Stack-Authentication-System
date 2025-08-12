8886ac1b843912ba0fcdc1bbd7a65aae9c39dceb
Ayush Yadav <theayush434@gmail.com>
2025-08-12 07:53:02 +0530

    Add MongoDB connection and CORS support; update dependencies

---
+++ backend/db/connectdb.js
@@ -0,0 +1,20 @@
+
+const mongoose = require('mongoose');
+
+require('dotenv').config(); // Load environment variables from .env file
+
+// Function to connect to MongoDB
+const connectDB = async () => {
+    try{
+
+        await mongoose.connect(process.env.MONGO_URI, {});
+       
+        console.log('MongoDB connected successfully');
+
+    } catch (error) {
+        console.error('MongoDB connection failed:', error.message);
+        process.exit(1); // Exit the process with failure
+    }   
+};
+
+module.exports = connectDB;
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,26 @@
 const express = require('express'); 
 const app = express();
+const connectDB = require('./db/connectdb');
+
+require('dotenv').config(); 
+connectDB();
+
+app.use(express.json({
+  limit: '50mb'
+})); // Middleware to parse JSON requests 
+
+app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded data
+// Middleware to parse URL-encoded data
+
+// Middleware to handle CORS    
+const cors = require('cors');
+app.use(cors({
+  origin: '*', // Allow all origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'] // Allow specific headers
+}));
+
+// Define a simple route  
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
